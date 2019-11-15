e4781a54cb0ed9b24d9d2c1e8cd1d5844d4ea1fd
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2019-11-15 23:30:59 +0900

    add last_name

--- hello_world_1114/app/models/user.rb
+++ hello_world_1114/app/models/user.rb
@@ -5,12 +5,13 @@ class User
     @hobby = "soccer"
     @blood_type = "B"
     @age = 32
+    @last?name = "sato"
   end
 
   def introduce
      <<~EOS
 
-     私の名前は#{@first_name}です。
+     私の名前は#{@last_name} + #{@first_name}です。
      誕生日は#{@birthday}で、趣味は#{@hobby}です。
      血液型は#{@blood_type}型です。
      年齢は#{@age}歳です。
