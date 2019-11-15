e84314f43352c5a1cb0124072b1caa4471ed6b42
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2019-11-15 23:20:43 +0900

    add age

--- hello_world_1114/app/models/user.rb
+++ hello_world_1114/app/models/user.rb
@@ -4,6 +4,7 @@ class User
     @birthday = "1987/06/19"
     @hobby = "soccer"
     @blood_type = "B"
+    @age = 32
   end
 
   def introduce
@@ -12,7 +13,7 @@ class User
      私の名前は#{@first_name}です。
      誕生日は#{@birthday}で、趣味は#{@hobby}です。
      血液型は#{@blood_type}型です。
-
+     年齢は#{@age}歳です。
      EOS
    end
- end 
+ end
